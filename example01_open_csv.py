76c5a160b94fd94d63f125367171315b52647df2
megan-procogia <93743426+megan-procogia@users.noreply.github.com>
2023-10-11 18:03:01 -0700

    Added more lines to example01_open_csv and added workspace conf
    Added nested lists to example01_open_csv.py and VSCode workspace conf

--- example01_open_csv.py
+++ example01_open_csv.py
@@ -2,7 +2,7 @@ import pandas
 
 
 # Let's start with some Python list comprehensions
-# Like apply() in R, list comprehensions allow for fast processing 
+# Like lapply() in R, list comprehensions allow for fast processing 
 # of items in a list without the need to use for loops
 list_of_consecutive_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 transformed_num = [x ** 3 for x in list_of_consecutive_num]
@@ -18,6 +18,46 @@ for x in list_of_consecutive_num:
     empty_list.append(x)
 
 print(*empty_list, '\n')
+
+
+###########################################
+# Let's try a more complicated structure 
+# Say you have a list of 'highest temp' recorded 
+# Ints within the same list are temperature measurements taken on consecutive days
+nestedLists = [[88,90,82,80,91,92,90,98,90,82,83,84], [101,88,85,85,86,80], [87,88,88,90,82,81]]
+print(nestedLists, '\n')
+
+# Which is the largest list in nestedLists? 
+the_largest = max(nestedLists, key=len)
+print(*the_largest, '\n')
+# How many days were measured?
+# print(len(the_largest))
+# What the hottest temp on this particular record?
+# print(max(the_largest))
+
+# What are the hottest 3 consecutive days? 
+# You can iterate via list comprehensions but I find for loops to make more sense
+for little_list in nestedLists:
+    
+    hottest = max(little_list)
+    ind = little_list.index(hottest)
+
+    # If hottest temp happened right at the start
+    if ind == 0:        
+        print('The hottest consecutive temps are: ' + str(little_list[ind:ind+3]))
+    elif ind == (len(little_list)+1): # if index is the last place        
+        print('The hottest consecutive temps are: ' + str(little_list[(ind-2):ind]))
+    # If index is not at last place but in middle    
+    elif little_list[ind-2] > little_list[ind+1] and ind != (len(little_list)+1):        
+        print('The hottest consecutive temps are: ' + str(little_list[(ind-2):(ind+1)]))
+    elif little_list[ind+1] > little_list[ind-2] and ind != (len(little_list)+1):        
+        print('The hottest consecutive temps are: ' + str(little_list[(ind-1):(ind+2)]))
+
+
+
+
+
+###########################################
 
 
 '''Since pandas is such an important and ubiquitous package 
